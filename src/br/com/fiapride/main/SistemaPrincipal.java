d5fcc1935c06fd34fb16987730ba9166c249c138
Júlia Marques <jumarques631@gmail.com>
2026-03-07 16:49:23 -0300

    feat: adiciona métodos de comportamento e atualiza README

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,38 +1,26 @@
 package br.com.fiapride.main;
 
-// import br.com.fiapride.model.Passageiro;
 import br.com.fiapride.model.Televisao;
 
 public class SistemaPrincipal {
 
     public static void main(String[] args) {
 
-//        Passageiro passageiro1 = new Passageiro();
-//        passageiro1.nome = "Ana Silva";
-//        passageiro1.saldo = 50.0;
-//
-//        // Criando o segundo passageiro (Objeto 2)
-//        Passageiro passageiro2 = new Passageiro();
-//        passageiro2.nome = "Carlos Souza";
-//        passageiro2.saldo = 12.50;
-//
-//        // Exibindo os dados no Console
-//        System.out.println("--- Sistema FiapRide ---");
-//        System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$" + passageiro1.saldo);
-//        System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$" + passageiro2.saldo);
+        // televisao01.estaLigada começa em false por padrão
+        Televisao televisao01 = new Televisao(23, 515);
 
-        Televisao televisao01 = new Televisao();
-        televisao01.marca = "Samsung";
-        televisao01.resolucaoEmPixels = 2160;
-        televisao01.tamanho = 50;
+        Televisao televisao02 = new Televisao(12,720);
+        televisao02.estaLigada = true;
 
-        Televisao televisao02 = new Televisao();
-        televisao02.marca = "LG";
-        televisao02.resolucaoEmPixels = 4320;
-        televisao02.tamanho = 32;
+        System.out.println("--- Televisão 01 ---");
+        televisao01.ligarTelevisao(televisao01.estaLigada);
+        televisao01.aumentarVolume(televisao01.estaLigada, 27);
 
-        System.out.println("Televisão 01 ---> Marca: " + televisao01.marca + " | Resolução em pixels: " + televisao01.resolucaoEmPixels + "p" + " | Tamanho: " + televisao01.tamanho + " polegadas");
-        System.out.println("Televisão 02 ---> Marca: " + televisao02.marca + " | Resolução em pixels: " + televisao02.resolucaoEmPixels + "p" +" | Tamanho: " + televisao02.tamanho + " polegadas");
+        System.out.println("--- Televisão 02 ---");
+        televisao02.ligarTelevisao(televisao02.estaLigada);
+        televisao02.aumentarVolume(televisao02.estaLigada, 0);
 
+        televisao02.ligarTelevisao(televisao02.estaLigada);
+        televisao02.aumentarVolume(televisao02.estaLigada, 110);
     }
 }
--- src/br/com/fiapride/model/Televisao.java
+++ src/br/com/fiapride/model/Televisao.java
@@ -1,7 +1,43 @@
 package br.com.fiapride.model;
 
 public class Televisao {
-    public String marca;
-    public int resolucaoEmPixels;
-    public int tamanho;
+    public int volume;
+    public int numeroCanal;
+    public boolean estaLigada;
+
+    // def __init__ em Python
+    public Televisao(int volume, int numeroCanal) {
+        this.volume = volume;
+        this.numeroCanal = numeroCanal;
+        this.estaLigada = false;
+
+    }
+
+    // def ligarTevelisao(estadoTV: boolean) em Python
+    public void ligarTelevisao(boolean estadoTV) {
+        if (!estadoTV) {
+            this.estaLigada = true;
+            System.out.println("Televisão ligada!");
+            return;
+        }
+
+        this.estaLigada = false;
+        System.out.println("Você desligou a televisão! Ela já estava ligada.");
+    }
+
+    // def aumentarVolume(estadoTV: boolean) em Python
+    public void aumentarVolume(boolean estadoTV, int volumeDesejado) {
+        if (estadoTV) {
+            if (volumeDesejado <= 0 || volumeDesejado >= 100) {
+                System.out.println("O volume deve ser um valor entre 0 a 100");
+                return;
+            }
+
+            this.volume = volumeDesejado;
+            System.out.println("Volume aumentado para: " + this.volume);
+            return;
+        }
+
+        System.out.println("A televisão está desligada! Ligue-a para conseguir aumentar o volume.");
+    }
 }
